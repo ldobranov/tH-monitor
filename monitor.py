88418c2fa7edbd7baf2eff16b281c0c76cad0e43
ldobranov <ldobranov@gmail.com>
2022-10-13 14:00:30 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,26 @@
 #! /usr/bin/env python
-import time
-import board
-import adafruit_dht
 import drivers
+from pigpio_dht import DHT22
 
 display = drivers.Lcd()
-dht1 = adafruit_dht.DHT22(board.D17)
-dht2 = adafruit_dht.DHT22(board.D27)
+sensor1 = DHT22(17)
+sensor2 = DHT22(27)
 
-try:
-  while True:
+while True:
     try:
-        tmp1 = dht1.temperature
-        hum1 = dht1.humidity
-        if (tmp1 == None or hum1 == None):
-            tmp1=float(1.1)
-            hum1=float(1.2)
-    except RuntimeError as error:
-        #print(error.args[0])
-        continue
-    try:
-        tmp2 = dht2.temperature
-        hum2 = dht2.humidity
-        if (tmp2 == None or hum2 == None):
-            tmp2=float(2.1)
-            hum2=float(2.2)
-    except RuntimeError as error:
-        #print(error.args[0])
-        continue
-    time.sleep(5)
-    print("T1:{:.1f}  H1:{}%  T2:{:.1f}  H2:{}%".format(tmp1, hum1, tmp2, hum2))
-    display.lcd_display_string("T1:{:.1f}  H1:{}% ".format(tmp1, hum1), 1)
-    display.lcd_display_string("T2:{:.1f}  H2:{}% ".format(tmp2, hum2), 2)
+        result1 = sensor1.sample(samples=3)
+        if (result1.get('valid') == True):
+            tmp1 = result1.get('temp_c')
+            hum1 = result1.get('humidity')
+            display.lcd_display_string("T1:{:.1f}  H1:{}% ".format(tmp1, hum1), 1)
+    except:
+        pass
 
-except KeyboardInterrupt:
-    display.lcd_clear()
+    try:
+        result2 = sensor2.sample(samples=3)
+        if (result2.get('valid') == True):
+            tmp2 = result2.get('temp_c')
+            hum2 = result2.get('humidity')
+            display.lcd_display_string("T2:{:.1f}  H2:{}% ".format(tmp2, hum2), 2)
+    except:
+        pass
