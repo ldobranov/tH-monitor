de9605c659d85deac50de79b70538e97cde9bc78
ldobranov <ldobranov@gmail.com>
2022-10-15 11:36:23 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,13 +1,35 @@
 #! /usr/bin/env python
+import logging
 import drivers
+import datetime
+import os
+import threading
 from pigpio_dht import DHT22
+from influxdb import client as influxdb
+#logmode = logging.DEBUG
+logmode = logging.WARNING
+
+logging.basicConfig(filename="/home/pi/tH-monitor/log_monitor.txt", level=logmode)
 
 sensor1 = DHT22(17)
 sensor2 = DHT22(27)
+
+#InfluxDB Connection Details
+influxHost = 'localhost'
+influxUser = 'admin'
+with open(os.path.dirname(os.path.abspath(__file__)) + '/secretstring', 'r') as f:
+    influxPasswd = f.readline().strip()
+f.close()
+
+old_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
+
 try:
     display = drivers.Lcd()
+    logging.warning(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "LCD started")
 except:
+    logging.error(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "LCD error")
     pass
+
 while True:
     try:
         result1 = sensor1.sample(samples=3)
@@ -15,7 +37,9 @@ while True:
             tmp1 = result1.get('temp_c')
             hum1 = result1.get('humidity')
             display.lcd_display_string("T1:{:.1f}  H1:{}% ".format(tmp1, hum1), 1)
+            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "sensor 1 readed")
     except:
+        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + " Sensor 1 error")
         pass
 
     try:
@@ -24,6 +48,34 @@ while True:
             tmp2 = result2.get('temp_c')
             hum2 = result2.get('humidity')
             display.lcd_display_string("T2:{:.1f}  H2:{}% ".format(tmp2, hum2), 2)
+            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "Sensor 2 readed")
     except:
+        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + " Sensor 2 error")
         pass
+    current_time = datetime.datetime.utcnow()
 
+    if ( current_time - datetime.timedelta(minutes=5) > old_time):
+     influxdbName = 'temperature'
+     old_time = current_time
+     influx_metric = [{
+      'measurement': 'TemperatureSensor',
+      'time': current_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+      'fields': {
+          'temperature1': tmp1,
+          'humidity1': hum1,
+          'temperature2': tmp2,
+          'humidity2': hum2
+       }
+     }]
+    logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "Start saving to db...")
+    #Saving data to InfluxDB
+    try:
+        db = influxdb.InfluxDBClient(influxHost, 8086, influxUser, influxPasswd, influxdbName)
+        db.write_points(influx_metric)
+        logging.info(current_time.strftime('%Y-%m-%dT%H:%M:%SZ') + "  Saved to db")
+
+    #finally:
+    #db.close()
+    except:
+        logging.error(current_time.strftime('%Y-%m-%dT%H:%M:%SZ') + "  ERROR Saving to db")
+        pass
