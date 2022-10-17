8990ec5b93280dabec865e5e457c20214db7956d
ldobranov <ldobranov@gmail.com>
2022-10-17 11:15:08 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -25,9 +25,9 @@ old_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
 
 try:
     display = drivers.Lcd()
-    logging.warning(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "LCD started")
+    logging.warning(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- LCD started")
 except:
-    logging.error(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "LCD error")
+    logging.error(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- LCD error")
     pass
 
 while True:
@@ -37,9 +37,9 @@ while True:
             tmp1 = result1.get('temp_c')
             hum1 = result1.get('humidity')
             display.lcd_display_string("T1:{:.1f}  H1:{}% ".format(tmp1, hum1), 1)
-            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "sensor 1 readed")
+            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- sensor 1 readed")
     except:
-        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + " Sensor 1 error")
+        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- Sensor 1 error")
         pass
 
     try:
@@ -48,34 +48,38 @@ while True:
             tmp2 = result2.get('temp_c')
             hum2 = result2.get('humidity')
             display.lcd_display_string("T2:{:.1f}  H2:{}% ".format(tmp2, hum2), 2)
-            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "Sensor 2 readed")
+            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- Sensor 2 readed")
     except:
-        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + " Sensor 2 error")
+        logging.debug(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- Sensor 2 error")
         pass
-    current_time = datetime.datetime.utcnow()
 
-    if ( current_time - datetime.timedelta(minutes=5) > old_time):
-     influxdbName = 'temperature'
-     old_time = current_time
-     influx_metric = [{
-      'measurement': 'TemperatureSensor',
-      'time': current_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-      'fields': {
-          'temperature1': tmp1,
-          'humidity1': hum1,
-          'temperature2': tmp2,
-          'humidity2': hum2
-       }
-     }]
-    logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "Start saving to db...")
     #Saving data to InfluxDB
     try:
-        db = influxdb.InfluxDBClient(influxHost, 8086, influxUser, influxPasswd, influxdbName)
-        db.write_points(influx_metric)
-        logging.info(current_time.strftime('%Y-%m-%dT%H:%M:%SZ') + "  Saved to db")
+        current_time = datetime.datetime.utcnow()
+        if ( current_time - datetime.timedelta(minutes=5) > old_time):
+            logging.info(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  -- Start saving to db...")
+            influxdbName = 'temperature'
+            old_time = current_time
+            influx_metric = [{
+                 'measurement': 'TemperatureSensor',
+                 'time': current_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                 'fields': {
+                     'temperature1': tmp1,
+                     'humidity1': hum1,
+                     'temperature2': tmp2,
+                     'humidity2': hum2
+                 }
+            }]
+            try:
+                db = influxdb.InfluxDBClient(influxHost, 8086, influxUser, influxPasswd, influxdbName)
+                db.write_points(influx_metric)
+                logging.info(current_time.strftime('%Y-%m-%dT%H:%M:%S') + "  -- Saved to db")
 
-    #finally:
-    #db.close()
+            except:
+                logging.error(current_time.strftime('%Y-%m-%dT%H:%M:%S') + "  -- ERROR Saving to db")
+                pass
+
+            #finally:
+            #db.close()
     except:
-        logging.error(current_time.strftime('%Y-%m-%dT%H:%M:%SZ') + "  ERROR Saving to db")
-        pass
+        logging.error(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "  --  NO LCD AND SENSORS CONECTED!!!")
